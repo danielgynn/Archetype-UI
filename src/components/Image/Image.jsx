216539e01416a2e4fc9ec4b378d502a2953ac767
Daniel Gynn <danielgynn94@gmail.com>
2020-05-23 16:12:38 +0100

    Bump version 0.28.6

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -11,6 +11,13 @@ const ImageContainer = styled.div`
     ${ props => borders(props) };
     ${ props => props.hide && hide(props, 2) };
 
+    ${ props => props.center && `
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    `};
+
     ${ breakpoint('md') `${ props => space(props, 1) }; ${ props => props.hide && hide(props, 1) };` }
     ${ breakpoint('lg') `${ props => space(props, 0) }; ${ props => props.hide && hide(props, 1) };` }
 `;
@@ -23,9 +30,10 @@ const StyledImage = styled.img`
 `;
 
 const Caption = styled.figcaption`
-    color: ${ props => props.theme.colors.textSecondary };
-    font-weight: 400;
-    font-size: .75rem;
+    color: ${props => props.title ? props.theme.colors.text : props.theme.colors.textSecondary};
+    font-weight: ${props => props.title ? 700 : 400};
+    font-size: ${props => props.title ? '1.2rem' : '.75rem'};
+    text-align: ${props => props.title ? 'center' : 'left'};
     display: block;
     margin-top: 5px;
 `;
@@ -53,14 +61,19 @@ export default class Image extends Component {
     }
 
     render() {
-        const { src, alt, width, height, style, caption, useExactSize, imageWidth, radius, fallback, onError, ...rest } = this.props;
+        const {
+            src, alt, width, height, style, caption, useExactSize, imageWidth, radius, fallback, onError,
+            titleCaption, center, ...rest
+        } = this.props;
         const { dimensions } = this.state;
 
         return (
             <ImageContainer
                 width={ width }
+                center={center}
                 { ...rest }
             >
+                { (caption && titleCaption) && <Caption title>{ caption } </Caption> }
                 <StyledImage
                     src={ src }
                     alt={ alt }
@@ -71,7 +84,7 @@ export default class Image extends Component {
                     onLoad={this.onImgLoad}
                     onError={ onError ? (e) => onError(e) : fallback ? (e) => this.addFallbackImage(e) : null }
                 />
-                { (caption) && <Caption>{ caption } </Caption> }
+                { (caption && !titleCaption) && <Caption>{ caption } </Caption> }
             </ImageContainer>
         )
     }
@@ -82,7 +95,8 @@ Image.defaultProps = {
     width: '150px',
     imageWidth: '100%',
     height: 'auto',
-    useExactSize: false
+    useExactSize: false,
+    titleCaption: false
 };
 
 Image.propTypes = {
@@ -94,5 +108,6 @@ Image.propTypes = {
     imageWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
     height: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array]),
     style: PropTypes.object,
-    useExactSize: PropTypes.bool
+    useExactSize: PropTypes.bool,
+    titleCaption: PropTypes.bool
 };
